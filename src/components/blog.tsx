e703fd0576a959f7436d48ec235cb3d6994be8bb
egely1337 <hyperamabanyiyen@gmail.com>
2023-12-08 18:17:43 +0300

    new blog post added

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -12,7 +12,7 @@ export default function Blog(props: {
         <>
             <Link href={props.link} className={`${props.className} w-auto p-4 flex flex-col justify-start items-start`}>
                 <div className="flex flex-col items-center">
-                    <img className="w-[684px] h-[514px] shadow-inset bg-black" src={props.img} alt="" />
+                    <img className="sm:w-[684px] sm:h-[514px] w-[384px] h-[214px] shadow-inset bg-black" src={props.img} alt="" />
                     <span className="text-gray-300 text-center text-sm text-opacity-90 mt-4">{props.title}</span>
                 </div>
             </Link>
