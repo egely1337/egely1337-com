312d001404438b6213c24d054cf7d302bf2ab3be
egely1337 <hyperamabanyiyen@gmail.com>
2023-12-10 13:30:56 +0300

    blog 🫏  animation added

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -2,17 +2,21 @@ import React from "react";
 
 import Link from "next/link";
 
+import { useInView } from "react-intersection-observer";
+
 export default function Blog(props: {
     link: string,
     title: string,
     img?: string,
     className?: string
 }) {
+    const [ref, inView] = useInView();
+
     return(
         <>
-            <Link href={props.link} className={`${props.className} w-auto p-4 flex flex-col justify-start items-start`}>
+            <Link ref={ref} href={props.link} className={`${props.className} w-auto p-4 flex flex-col justify-start items-start duration-[1s] ${inView ? "" : "translate-y-[20px] opacity-0"}`}>
                 <div className="flex flex-col items-center">
-                    <img className="sm:w-[684px] sm:h-[514px] w-[384px] h-[214px] shadow-inset bg-black" src={props.img} alt="" />
+                    <img className="sm:w-[684px] sm:h-[514px] w-[384px] h-[280px] shadow-inset bg-black" src={props.img} alt="" />
                     <span className="text-gray-300 text-center text-sm text-opacity-90 mt-4">{props.title}</span>
                 </div>
             </Link>
