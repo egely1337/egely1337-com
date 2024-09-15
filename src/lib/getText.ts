887e677d43409f99346d156ed9030589525f1a5d
egely1337 <hyperamabanyiyen@gmail.com>
2024-09-15 14:10:52 +0300

    hello

--- src/lib/getText.ts
+++ src/lib/getText.ts
@@ -1,6 +1,11 @@
 export default function getText(text: string) {
     if(text.length < 300) return text;
     else {
-        return `${text.split(" ").slice(0, 20).join(' ')}...`
+        return `${
+            text
+            .split(" ")
+            .slice(0, 20)
+            .join(' ')
+            .replace(',', '')}...`
     }
 }
